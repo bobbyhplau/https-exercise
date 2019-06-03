8ff4831e7b40e3d02b98a3f8c657804da919e5dd
Bobby <bobbyhplau@gmail.com>
2019-06-03 20:54:13 +0000

    getAndPrintHTML now takes 2 arguments

---
+++ get-html.js
@@ -0,0 +1,31 @@
+var https = require("https");
+
+function getAndPrintHTML(options, callback) {
+
+  var buffer = '';
+
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+
+      buffer += data;
+    });
+
+    response.on('end', function() {
+      printHTML(buffer);
+    });
+  });
+
+}
+
+function printHTML(html) {
+  console.log(html);
+}
+
+
+var requestOptions = {
+  host: 'sytantrix.github.io',
+  path: '/http-examples/step4.html'
+};
